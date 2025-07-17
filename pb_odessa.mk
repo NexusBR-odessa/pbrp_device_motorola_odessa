c220bde34f8e75c294e3ba7512442e7beb08fe7c
NexusBR-odessa <miguel03barreto@gmail.com>
2025-07-17 17:13:03 -0300

    Mais algumas correções v3

--- pb_odessa.mk
+++ pb_odessa.mk
@@ -1,8 +1,8 @@
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk) -- only for 64bit phones
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit from device
-$(call inherit-product, device/motorola/odessa/device.mk) -- path to main device makefile
+$(call inherit-product, device/motorola/odessa/device.mk)
 
 # Inherit common product files.
 $(call inherit-product, vendor/pb/config/common.mk)
